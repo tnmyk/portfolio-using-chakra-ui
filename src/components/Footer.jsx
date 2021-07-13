4d2b3cac63db502c188bf3078d74d29d9e28787e
Tanmaykk <tanmaykachroo@gmail.com>
2021-07-13 19:59:19 +0530

    Responsive improvement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import {
-  Button,
   Flex,
   Icon,
   Link,
   Text,
+  useColorMode,
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react';
@@ -11,25 +11,30 @@ import { AiOutlineTwitter, AiOutlineGithub } from 'react-icons/ai';
 const Footer = () => {
   return (
     <Flex
-      w="60%"
+      w={['90%', '60%']}
       mx="auto"
       mt="7rem"
       borderTop="1px"
       borderColor={useColorModeValue('gray.400', 'gray.500')}
       flexDir={['column-reverse', 'row']}
+      color={useColorModeValue('gray.600', 'gray.300')}
     >
-      <VStack spacing="2rem" alignItems="start" p="3rem" pt={['0','3rem']} w={['100%', '50%']}>
-        <Button>
-          <Link to="/">
-            Twitter <Icon ml="1px" mb="2px" as={AiOutlineTwitter} />
-          </Link>
-        </Button>
-        <Button>
-          <Link to="/">
-            Github
-            <Icon ml="3px" mb="2px" as={AiOutlineGithub} />{' '}
-          </Link>
-        </Button>
+      <VStack
+        spacing="2rem"
+        alignItems="start"
+        p="3rem"
+        pt={['0', '3rem']}
+        w={['100%', '50%']}
+      >
+        <Link to="/">
+          Twitter <Icon ml="1px" mb="2px" as={AiOutlineTwitter} />
+        </Link>
+
+        <Link to="/">
+          Github
+          <Icon ml="3px" mb="2px" as={AiOutlineGithub} />{' '}
+        </Link>
+
         <Text>Email: tanmaykachroo@gmail.com</Text>
       </VStack>
       <VStack
@@ -38,12 +43,11 @@ const Footer = () => {
         alignItems="start"
         justifyContent="center"
         p="3rem"
+        pb={['2rem', '3rem']}
       >
-        <Button>
-          <Link fontSize="lg" to="/">
-            Resume 📗
-          </Link>
-        </Button>
+        <Link fontSize="lg" to="/">
+          Resume 📗
+        </Link>
       </VStack>
     </Flex>
   );
