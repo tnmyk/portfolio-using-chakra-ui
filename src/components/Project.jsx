75c48465f0df0f493a4c6d3d9a59544dba4b1105
Tanmaykk <tanmaykachroo@gmail.com>
2021-07-13 21:07:40 +0530

    improveds project ui

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,27 +4,39 @@ const Project = ({ title, snippet }) => {
   const snippetColor = useColorModeValue('gray.700', 'gray.300');
   const titleColor = useColorModeValue('gray.800', 'gray.200');
   return (
-    <RouterLink to={`/projects/${title.replace(/\s+/g, '-').toLowerCase()}`}>
-      <Box
-        mx="1.5rem"
-        my="2rem"
-        boxShadow="base"
-        p="6"
-        borderRadius="lg"
-        border="1px"
-        borderColor={useColorModeValue('gray.200', 'gray.600')}
-        _hover={{
-          borderColor: useColorModeValue('gray.300', 'gray.500'),
-        }}
-      >
-        <Text fontWeight="medium" fontSize="1.5rem" color={titleColor}>
-          {title}
-        </Text>
-        <Text ml="0.1rem" color={snippetColor}>
-          {snippet}
-        </Text>
-      </Box>{' '}
-    </RouterLink>
+    <Box
+      mx="1.5rem"
+      my="2rem"
+      boxShadow="base"
+      p="6"
+      borderRadius="lg"
+      border="1px"
+      borderColor={useColorModeValue('gray.200', 'gray.600')}
+      _hover={{
+        borderColor: useColorModeValue('gray.300', 'gray.500'),
+      }}
+    >
+      <Text fontWeight="medium" fontSize="1.5rem" color={titleColor}>
+        {title}
+      </Text>
+      <Text ml="0.1rem" color={snippetColor}>
+        {snippet}
+      </Text>
+      <Text>
+        <RouterLink
+          to={`/projects/${title.replace(/\s+/g, '-').toLowerCase()}`}
+        >
+          <Text
+            mt="1rem"
+            fontWeight="bold"
+            w="max-content"
+            textDecor="underline"
+          >
+            See Details
+          </Text>
+        </RouterLink>
+      </Text>
+    </Box>
   );
 };
 
