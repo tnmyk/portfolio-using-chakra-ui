87d9444638075805dde809321e8e7099315aff8d
tnmyk <73903781+tnmyk@users.noreply.github.com>
2021-07-19 18:19:54 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     snippet: (
       <>
         Currently under development using <strong>Next.js</strong>, <strong>React.js</strong>, <strong>Chakra UI</strong>. 
-         In the app students can mark their progress using the checklist tab, learn from the code snippets, visualize the topics through the graph.
+         In the app students can mark their progress using the topic-wise checklist tab, learn from the code snippets and visualize the topics through the graph. 
         <strong>More to come soon.</strong>
       </>
     ),
