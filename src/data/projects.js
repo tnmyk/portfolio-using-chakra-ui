03a20d06a7847ddefc01addc7c5b8c7d5f9f5edd
tnmyk <73903781+tnmyk@users.noreply.github.com>
2021-07-19 18:15:25 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,24 @@
 export const projects = [
   {
+     {
+    title: 'Learn DSA Web App(MVP ready)',
+    snippet: (
+      <>
+        Currently under development using <strong>Next.js</strong>, <strong>React.js</strong>, <strong>Chakra UI</strong>. 
+         In the app students can mark their progress using the checklist tab, learn from the code snippets, visualize the topics through the graph.
+        <strong>More to come soon.</strong>
+      </>
+    ),
+  },
     title: 'Portfolio Builder',
     snippet: (
       <>
         Made using <strong>React.js</strong>, <strong>Firebase</strong>. This
         lets you create your own responsive portfolio website easily in few
         minutes and lets you display your projects, skills, social-media links
-        and other import details.
+        and other important details.
       </>
     ),
   },
+ 
 ];
