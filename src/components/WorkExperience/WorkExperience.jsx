7bff78c179feeb724fa1f825a890920b612076cf
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-26 23:14:04 +0530

    redeploy

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -8,18 +8,18 @@ const WorkExperience = ({ workExperience }) => {
     <Box border="1px solid gray" px="7" py="6" pb="8" borderRadius="4">
       <Text fontWeight="medium" fontSize="1.4rem" color={titleColor}>
         {workExperience.position}{' '}
-        <Text
-          display="inline"
-          verticalAlign="3px"
-          fontSize="0.9rem"
-          ml="0.1rem"
-          color={workExperience.snippetColor}
-        >
-          ({workExperience.duration})
-        </Text>
       </Text>
       <Text fontSize="1.1rem" ml="1" color={titleColor}>
         <i>{workExperience.companyName}</i>
+      </Text>
+      <Text
+        display="inline"
+        verticalAlign="3px"
+        fontSize="0.9rem"
+        ml="0.1rem"
+        color={workExperience.snippetColor}
+      >
+        ({workExperience.duration})
       </Text>
       <Text ml="0.1rem" mt="3" color={snippetColor}>
         {workExperience.description}
--- src/components/WorkExperience/WorkExperienceContainer.jsx
+++ src/components/WorkExperience/WorkExperienceContainer.jsx
@@ -4,9 +4,8 @@ import workExperiences from './workExperiences';
 const WorkExperienceContainer = () => {
   return (
     <Box ml="1.5rem" mt="1rem" pt="2rem">
-      {workExperiences.map(workExperience => {
-        console.log(workExperience);
-        return <WorkExperience workExperience={workExperience} />;
+      {workExperiences.map((workExperience, index) => {
+        return <WorkExperience key={index} workExperience={workExperience} />;
       })}
     </Box>
   );
