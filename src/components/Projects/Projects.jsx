77a94218059febbd732503b8b89c71c192947df0
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-27 22:42:59 +0530

    link to github profile added to projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,27 @@
 import { Box, Heading } from '@chakra-ui/react';
 import ProjectsContainer from './ProjectsContainer';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 const Timeline = () => {
   return (
     <Box mt="3rem">
       <Heading>My Products</Heading>
-      <ProjectsContainer limit='1000'/>
+      <ProjectsContainer limit="1000" />
+
+      <Box textAlign='center'>
+
+      View more of my projects on{' '}
+      <a target="_blank" rel="noreferrer" href="https://github.com/tnmyk" style={{textDecoration:'underline'}}>
+       my Github
+        <AiOutlineGithub
+          style={{
+            display: 'inline-flex',
+            marginLeft: '0.2rem',
+            verticalAlign: '-2px',
+          }}
+        />
+      </a>
+      </Box>
     </Box>
   );
 };
