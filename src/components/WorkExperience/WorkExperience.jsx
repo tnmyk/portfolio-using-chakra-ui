319d28a05ce4fa1844ea74620eeb7ff851365374
Tanmaykk <tanmaykachroo@gmail.com>
2021-12-18 18:29:07 +0530

    Added margin to experience components

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -5,7 +5,7 @@ const WorkExperience = ({ workExperience }) => {
   const snippetColor = useColorModeValue('gray.700', 'gray.300');
   const titleColor = useColorModeValue('gray.800', 'gray.200');
   return (
-    <Box border="1px solid gray" px="7" py="6" pb="8" borderRadius="4">
+    <Box border="1px solid gray" px="7" py="6" pb="8" mb="3" borderRadius="4">
       <Text fontWeight="medium" fontSize="1.4rem" color={titleColor}>
         {workExperience.position}{' '}
       </Text>
